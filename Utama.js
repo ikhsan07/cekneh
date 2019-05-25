56ffa382adcba725ca09ad2a8b14c0c6a73b1dae
ikhsan07 <ikhsanmiftah123@gmail.com>
2019-05-25 07:54:36 +0700

    Update Utama.js

--- Utama.js
+++ Utama.js
@@ -31,7 +31,7 @@ if(x=="7"){xxx="7.html"}
 if(x=="8"){xxx="8.html"}
 if(x=="9"){xxx="test.html"}
 if(x=="10"){xxx="10.html"}
-output.value = "https://downloadlanjut.blogspot.com/2018/08/"}a_to_ve[i].href="https://go.oprekersphone.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+a_to_ve[i].href="https://downloadlanjut.blogspot.com/2018/08/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
